3c13408fbca2b41b26bdcb85291d140ca8a682c1
Olamide <soluade101@gmail.com>
2022-07-20 11:42:07 +0100

    refactor schema to provide instances of CRUD operations

--- schemas/techtalk.py
+++ schemas/techtalk.py
@@ -4,18 +4,7 @@ from typing import Optional
 from sqlalchemy.sql import func
 
 
-class TechTalkIn(BaseModel):
-    title: str
-    description: str
-    thumbsup: int
-    completed: bool = False
-    timeoftalk: Optional[datetime]
-
-    class Config:
-        orm_mode = True
-
-
-class TechTalk(BaseModel):
+class TechTalkBase(BaseModel):
     id:  int
     title: str
     description: str
@@ -25,3 +14,28 @@ class TechTalk(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class TechTalkCreate(TechTalkBase):
+    title: str
+
+
+class TechTalkUpdate(TechTalkBase):
+    pass
+
+
+class TechTalkInDBBase(TechTalkBase):
+    id:  int
+    title: str
+    # owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class TechTalk(TechTalkInDBBase):
+    pass
+
+
+class TechTalkInDB(TechTalkInDBBase):
+    pass
--- schemas/token.py
+++ schemas/token.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class TokenPayload(BaseModel):
+    sub: Optional[int] = None
 
 
 class TokenData(BaseModel):
